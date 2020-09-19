61184667f67f966a1dfb9f02eeb87d71f5e23a41
BurkeO <burkeowen98@gmail.com>
2020-09-19 19:09:51 +0100

    Initial Commit of countries.txt and python script to pull recordings from Xeno Canto
    Signed-off-by: BurkeO <burkeowen98@gmail.com>

---
+++ main.py
@@ -0,0 +1,45 @@
+import argparse
+from requests import get
+import time
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+API_URL = "https://www.xeno-canto.org/api/2/recordings"
+
+
+def main(args: argparse.Namespace) -> None:
+    with open(args.countries_file) as countries_file:
+        countries_list = countries_file.read().splitlines()
+        for country in countries_list:
+            print(f'Current country = {country}')
+            if " " in country:
+                country = f'\"{country}\"'
+            params = {'query': f'cnt:{country}'}
+            response = get(url=API_URL, params=params)
+            if response.status_code != 200:
+                print(f'Error code for country = {country}')
+            elif not response.json()['recordings']:
+                print(f'Empty recordings for country = {country}')
+            json_response_data = response.json()
+            # print(json_response_data)
+            # for recording in json_response_data['recordings']:
+            #     print(f'recording = {recording}')
+            time.sleep(1.5)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser("Pull audio files from Xeno Canto API using countries list file")
+    parser.add_argument('-c', '--countries_file', type=str,
+                        help="""A file path to a txt file containing a list of countries separated by line breaks""")
+    parser.add_argument('-o', '--output_dir', type=str,
+                        help="""A path to the output directory to store the pulled audio file""")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_args())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
