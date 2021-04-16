c6e2f237ca74ef385facba221c5f1e93af5b1fa3
Owen Burke <35539701+BurkeO@users.noreply.github.com>
2021-04-16 18:17:16 +0100

    adding changes to repet pip - is working now
    Signed-off-by: Owen Burke <35539701+BurkeO@users.noreply.github.com>

--- Test_Repet_Pip/main.py
+++ Test_Repet_Pip/main.py
@@ -2,11 +2,16 @@ import nussl
 import matplotlib.pyplot as plt
 import time
 
+from nussl.core.utils import audio_signals_to_musdb_track
+
+import scipy
+
 
 def main():
     start_time = time.time()
 
     audio_path = 'Northern_Raven_4.wav'
+    sample_rate, _ = scipy.io.wavfile.read(audio_path)
     audio_signal = nussl.AudioSignal(audio_path)
     separator = nussl.separation.primitive.Repet(
         audio_signal, mask_type='binary')
@@ -22,7 +27,13 @@ def main():
         'Background': estimates[0], 'Foreground': estimates[1]},
         show_legend=False)
     plt.show()
-    nussl.play_utils.multitrack(estimates, ['Background', 'Foreground'])
+    # nussl.play_utils.multitrack(estimates, ['Background', 'Foreground'])
+
+    estimates[0].write_audio_to_file("background.wav")
+    estimates[1].write_audio_to_file("foreground.wav")
+
+    # scipy.io.wavfile.write('background_signal.wav', sample_rate, estimates[0])
+    # scipy.io.wavfile.write('foreground_signal.wav', sample_rate, estimates[1])
 
 
 if __name__ == '__main__':
